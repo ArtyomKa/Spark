837ce4ed21583d588322a024870fd2d803630500
Artyom Katsap <artyom.katsap@gmail.com>
2017-01-24 21:38:56 +0200

    fixing local dev  env

--- config/default.js
+++ config/default.js
@@ -4,7 +4,7 @@ console.log("default.js > " + JSON.stringify(opsworks));
 console.log("NODE_ENV " + process.env.NODE_ENV);
 
 module.exports =  {
-  database: opsworks.db[process.env.NODE_ENV],
+  database: process.env.NODE_ENV ? opsworks.db[process.env.NODE_ENV ] : opsworks.db.development,
   
   server: opsworks.server,
 
